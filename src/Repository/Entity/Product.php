73f7c263a03dcc2683b6b64dff473d5931046788
artem314 <artem_pochta_314@mail.ru>
2025-02-14 17:35:01 +0300

    fix Product.php

--- src/Repository/Entity/Product.php
+++ src/Repository/Entity/Product.php
@@ -1,21 +1,34 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Raketa\BackendTestTask\Repository\Entity;
 
-readonly class Product
+class Product
 {
+    private int $price;
+    private bool $isActive;
+    private string $category;
+    private string $name;
+    private string $description;
+    private string $thumbnail;
+
     public function __construct(
-        private int $id,
-        private string $uuid,
-        private bool $isActive,
-        private string $category,
-        private string $name,
-        private string $description,
-        private string $thumbnail,
-        private float $price,
+        private readonly int $id,
+        private readonly string $uuid,
+        bool $isActive,
+        string $category,
+        string $name,
+        string $description,
+        string $thumbnail,
+        int $price,
     ) {
+        $this->thumbnail = $thumbnail;
+        $this->description = $description;
+        $this->name = $name;
+        $this->category = $category;
+        $this->isActive = $isActive;
+        $this->price = $price;
     }
 
     public function getId(): int
@@ -33,9 +46,19 @@ readonly class Product
         return $this->isActive;
     }
 
+    public function setIsActive(bool $isActive): void
+    {
+        $this->isActive = $isActive;
+    }
+
     public function getCategory(): string
     {
         return $this->category;
+    }
+
+    public function setCategory(string $category): void
+    {
+        $this->category = $category;
     }
 
     public function getName(): string
@@ -43,9 +66,19 @@ readonly class Product
         return $this->name;
     }
 
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
     public function getDescription(): string
     {
         return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
     }
 
     public function getThumbnail(): string
@@ -53,8 +86,18 @@ readonly class Product
         return $this->thumbnail;
     }
 
-    public function getPrice(): float
+    public function setThumbnail(string $thumbnail): void
+    {
+        $this->thumbnail = $thumbnail;
+    }
+
+    public function getPrice(): int
     {
         return $this->price;
+    }
+
+    public function setPrice(int $price): void
+    {
+        $this->price = $price;
     }
 }
