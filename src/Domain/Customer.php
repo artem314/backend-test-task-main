9e52a18aefe636381785783b2aee5070a2dd4979
artem314 <artem_pochta_314@mail.ru>
2025-02-14 17:34:10 +0300

    fix Customer.php

--- src/Domain/Customer.php
+++ src/Domain/Customer.php
@@ -1,18 +1,27 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Raketa\BackendTestTask\Domain;
 
-final readonly class Customer
+final class Customer
 {
+    private string $firstName;
+    private string $lastName;
+    private string $middleName;
+    private string $email;
+
     public function __construct(
-        private int $id,
-        private string $firstName,
-        private string $lastName,
-        private string $middleName,
-        private string $email,
+        private readonly int $id,
+        string $firstName,
+        string $lastName,
+        string $middleName,
+        string $email,
     ) {
+        $this->email = $email;
+        $this->middleName = $middleName;
+        $this->lastName = $lastName;
+        $this->firstName = $firstName;
     }
 
     public function getId(): int
@@ -25,9 +34,19 @@ final readonly class Customer
         return $this->firstName;
     }
 
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
     public function getLastName(): string
     {
         return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
     }
 
     public function getMiddleName(): string
@@ -35,8 +54,29 @@ final readonly class Customer
         return $this->middleName;
     }
 
+    public function setMiddleName(string $middleName): void
+    {
+        $this->middleName = $middleName;
+    }
+
     public function getEmail(): string
     {
         return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getFullName(): string
+    {
+        $fullname = implode(' ', [
+            $this->getLastName(),
+            $this->getFirstName(),
+            $this->getMiddleName(),
+        ]);
+
+        return $fullname;
     }
 }
